053a0b919a718e1bf5579690c722fceca31ccc52
dlwogus552 <145645760+dlwogus552@users.noreply.github.com>
2024-04-08 10:03:40 +0900

    news 최종

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: false,
+    async rewrites() {
+      return [
+        {
+          source: "/:path*",
+          destination: "https://openapi.naver.com/:path*",
+        },
+      ];
+    },
+  };
 
 module.exports = nextConfig
